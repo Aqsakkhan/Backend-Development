86a642801d56f77cae83b163dbfde9613be1f7a8
Khan Aqsa Khurshid Alam <aqsakkhan04@gmail.com>
2026-08-11 15:37:02 +0000

    feat: implement CRUD API endpoints for notes management

---
+++ src/app.js
@@ -0,0 +1,43 @@
+const express = require('express');
+
+const app = express()
+app.use(express.json());
+
+const notes = []
+
+app.post('/notes', (req, res) => {
+    notes.push(req.body)
+
+    res.status(201).json({
+        message : "Note created successfully"
+    })
+})
+
+app.get('/notes', (req, res) => {
+    res.status(200).json({
+        message : "Notes fetched successfully",
+        notes : notes
+    })
+})
+
+app.delete('/notes/:index', (req, res) => {
+    const index = req.params.index
+
+    delete notes[index]
+
+    res.status(201).json({
+        message : "Note deleted successfully"
+    })
+})
+
+app.put('/notes/:index', (req, res) => {
+    const index = req.params.index
+    const description = req.body.description
+
+    notes[index].description = description
+
+    res.status(200).json({
+        message : "Note updated successfully"
+    })
+})
+module.exports = app
