a4a3ec3e79da4208f4d5439bf116985aa27a1122
Khan Aqsa Khurshid Alam <aqsakkhan04@gmail.com>
2026-08-13 06:06:32 +0000

    feat : Initial setup for project

---
+++ MiniProject/src/db/db.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose')
+
+async function connectDB() {
+    try {
+        await mongoose.connect(process.env.MONGODB_URL)
+        console.log("MongoDB connected successfully");
+    } catch (error) {
+        console.log("MongoDB connection failed!");
+        process.exit(1);
+    }
+    
+}
+
+module.exports = connectDB
+
